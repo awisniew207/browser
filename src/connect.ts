78a58523da45f000ccc809f781dde8cd5cf9d281
awisniew207 <andrew.wisniewski.08@gmail.com>
2024-11-11 11:03:06 -0800

    testing for TG user

--- src/connect.ts
+++ src/connect.ts
@@ -1,4 +1,11 @@
 import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import {
+  LitAbility,
+  LitAccessControlConditionResource,
+  createSiweMessage,
+  generateAuthSig,
+} from "@lit-protocol/auth-helpers";
+import * as ethers from "ethers";
 
 export const connectToLit = async () => {
     try {
@@ -10,6 +17,42 @@ export const connectToLit = async () => {
 
       await litNodeClient.connect();
       console.log('Connected to Lit Network');
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const ethersSigner = provider.getSigner();
+      console.log("Connected account:", await ethersSigner.getAddress());
+      
+      const sessionSignatures = await litNodeClient.getSessionSigs({
+        chain: "ethereum",
+        expiration: new Date(Date.now() + 1000 * 60 * 10 ).toISOString(), // 10 minutes
+        resourceAbilityRequests: [
+          {
+            resource: new LitAccessControlConditionResource("*"),
+            ability: LitAbility.AccessControlConditionDecryption,
+          },
+        ],
+        authNeededCallback: async ({
+          uri,
+          expiration,
+          resourceAbilityRequests,
+        }) => {
+          const toSign = await createSiweMessage({
+            uri,
+            expiration,
+            resources: resourceAbilityRequests,
+            walletAddress: await ethersSigner.getAddress(),
+            nonce: await litNodeClient.getLatestBlockhash(),
+            litNodeClient,
+          });
+      
+          return await generateAuthSig({
+            signer: ethersSigner,
+            toSign,
+          });
+        },
+      });
+      console.log("Session signatures:", sessionSignatures);
     } catch (error) {
       console.error('Failed to connect to Lit Network:', error);
     }
